575b430b85922f39826cd70d15aac01d6c3c7884
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2020-09-27 20:32:19 +0900

    generators

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module KutstuSample
   class Application < Rails::Application
+    config.generators do |g|
+      g.assets false
+      g.halper false
+      g.test_framework false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
